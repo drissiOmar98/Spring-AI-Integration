a78dc1a9cc087da145f185958b495f3e3e86d52e
DrissiOmar <omar.drissi@esprit.tn>
2025-11-23 21:27:58 +0100

    chore: initialize Spring Boot project with Spring AI setup

---
+++ spring-with-ai/src/main/java/com/omar/spring_with_ai/SpringWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithAiApplication.class, args);
+	}
+
+}
---
+++ spring-with-ai/src/test/java/com/omar/spring_with_ai/SpringWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
