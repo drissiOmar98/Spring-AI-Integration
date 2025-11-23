dda4bace17f0e9c1b52ac2e931bb399c6bbaab8f
DrissiOmar <omar.drissi@esprit.tn>
2025-11-23 21:51:02 +0100

    chore(init): initialize Spring Boot project with base structure for AI-powered chat application

---
+++ spring-ai-02-chat-with-llms/src/main/java/com/omar/spring_ai_02_chat_with_llms/SpringAi02ChatWithLlmsApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_02_chat_with_llms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi02ChatWithLlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAi02ChatWithLlmsApplication.class, args);
+	}
+
+}
