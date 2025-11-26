a1df60b6dab09aa15a182b93a06cf147f992f5bd
DrissiOmar <omar.drissi@esprit.tn>
2025-11-26 20:15:00 +0100

    chore(init): initialize project for Spring AI integration with Anthropic Claude models

---
+++ spring-ai-03-chat-with-claude/src/main/java/com/omar/spring_ai_03_chat_with_claude/ChatWithClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_03_chat_with_claude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWithClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWithClaudeApplication.class, args);
+	}
+
+}
