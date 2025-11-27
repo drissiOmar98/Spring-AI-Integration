321b2fb33a17d90406b10f2e447bb91d65ea4ecf
DrissiOmar <omar.drissi@esprit.tn>
2025-11-27 20:00:06 +0100

    feat(prompt-templates): create new Spring AI  project showcasing PromptTemplate usage

---
+++ prompt-templates/src/main/java/com/omar/prompt_templates/PromptTemplatesApplication.java
@@ -0,0 +1,13 @@
+package com.omar.prompt_templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptTemplatesApplication.class, args);
+	}
+
+}
