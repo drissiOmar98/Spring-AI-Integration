f09962eb7dda7afdf3572550f73a87608cb7e0bc
DrissiOmar <omar.drissi@esprit.tn>
2025-11-30 16:51:07 +0100

    feat(rag): initialize Spring AI RAG project with vector store advisor integration

---
+++ spring-ai-rag-vector-store/src/main/java/com/omar/spring_ai_rag_vector_store/SpringAiRagVectorStoreApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_rag_vector_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagVectorStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagVectorStoreApplication.class, args);
+	}
+
+}
---
+++ spring-ai-rag-vector-store/src/test/java/com/omar/spring_ai_rag_vector_store/SpringAiRagVectorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_rag_vector_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagVectorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
