c85a0a800a54a13f66f576d9132ad394c2492162
DrissiOmar <omar.drissi@esprit.tn>
2025-12-11 16:45:28 +0100

    feat(tool-calling): initialize Spring AI Tool Calling project with external tool integration

---
+++ tool-calling/src/test/java/com/omar/tool_calling/ToolCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.tool_calling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCallingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
