f3f9627d79a6246ab0059b59627dcffe249d4fef
DrissiOmar <omar.drissi@esprit.tn>
2026-01-05 21:25:30 +0100

    feat(rag-finance): initialize Spring AI project for financial document analysis using RAG and pgvector

---
+++ spring-ai-financial-rag/src/main/java/com/omar/spring_ai_financial_rag/SpringAiFinancialRagApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_financial_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiFinancialRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiFinancialRagApplication.class, args);
+	}
+
+}
---
+++ spring-ai-financial-rag/src/test/java/com/omar/spring_ai_financial_rag/SpringAiFinancialRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_financial_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFinancialRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
