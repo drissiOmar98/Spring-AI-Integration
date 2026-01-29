9b0db91ff7cc69190c4caef1042b9d2f2b935887
DrissiOmar <omar.drissi@esprit.tn>
2026-01-29 19:39:03 +0100

    feat: initialize Native Structured Output demo with Spring AI

---
+++ native-structured-output/src/main/java/com/omar/native_structured_output/NativesoApplication.java
@@ -0,0 +1,13 @@
+package com.omar.native_structured_output;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeStructuredOutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeStructuredOutputApplication.class, args);
+	}
+
+}
---
+++ native-structured-output/src/test/java/com/omar/native_structured_output/NativesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.native_structured_output;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
