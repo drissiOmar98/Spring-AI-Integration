6d0ba36029b1d03af3f09bd3287fde135697ec93
DrissiOmar <omar.drissi@esprit.tn>
2026-02-04 16:08:43 +0100

    feat(web-search): initialize real-time web search project with Spring AI

---
+++ spring-ai-web-search/src/main/java/com/omar/spring_ai_web_search/SpringAiWebSearchApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_web_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiWebSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiWebSearchApplication.class, args);
+	}
+
+}
