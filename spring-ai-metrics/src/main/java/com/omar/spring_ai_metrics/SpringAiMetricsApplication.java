f797019cec06d3a08e4ce2acac192f77d56310c1
DrissiOmar <omar.drissi@esprit.tn>
2026-02-17 16:47:19 +0100

    chore(init): bootstrap Spring Boot project for AI token usage monitoring & observability via Actuator, Spring AI & Prometheus

---
+++ spring-ai-metrics/src/main/java/com/omar/spring_ai_metrics/SpringAiMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMetricsApplication.class, args);
+	}
+
+}
