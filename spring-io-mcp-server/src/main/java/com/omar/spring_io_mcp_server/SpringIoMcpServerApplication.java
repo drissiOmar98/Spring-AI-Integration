8c1241dac00ff4172157f3d52396be21ffad9659
DrissiOmar <omar.drissi@esprit.tn>
2026-02-20 00:12:05 +0100

    chore: bootstrap Spring I/O MCP Server project

---
+++ spring-io-mcp-server/src/main/java/com/omar/spring_io_mcp_server/SpringIoMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_io_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoMcpServerApplication.class, args);
+	}
+
+}
---
+++ spring-io-mcp-server/src/test/java/com/omar/spring_io_mcp_server/SpringIoMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_io_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
