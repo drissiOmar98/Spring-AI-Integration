0cbc17b5486686684b59919f96ab4d5283e933d2
DrissiOmar <omar.drissi@esprit.tn>
2026-02-22 16:15:31 +0100

    feat(mcp-client): bootstrap MCP STDIO client with Spring AI and Ollama model

---
+++ mcp-client-stdio/src/main/java/com/omar/mcp_client_stdio/McpClientStdioApplication.java
@@ -0,0 +1,13 @@
+package com.omar.mcp_client_stdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpClientStdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpClientStdioApplication.class, args);
+	}
+
+}
---
+++ mcp-client-stdio/src/test/java/com/omar/mcp_client_stdio/McpClientStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.mcp_client_stdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpClientStdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
