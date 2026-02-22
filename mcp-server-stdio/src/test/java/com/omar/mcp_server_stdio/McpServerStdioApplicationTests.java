130a9eed246c120fde5c1e0aceec933e92b691fd
DrissiOmar <omar.drissi@esprit.tn>
2026-02-22 15:19:58 +0100

    feat(mcp): bootstrap MCP STDIO server with Spring AI

---
+++ mcp-server-stdio/src/test/java/com/omar/mcp_server_stdio/McpServerStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.mcp_server_stdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerStdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
